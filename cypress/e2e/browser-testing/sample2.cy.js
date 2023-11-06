ccb293992876962b8c028404b7f38ca73a72c448
torga <torgaigor@gmai.com>
2023-11-06 01:33:07 -0300

    adding best practices in browser testing

--- cypress/e2e/browser-testing/sample2.cy.js
+++ cypress/e2e/browser-testing/sample2.cy.js
@@ -3,12 +3,10 @@ describe('Browser testing bad practice - anchor with target _blank', () => {
     cy.visit('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
   })
 
-  it('directs the user to the privacy page after removing the target and clicking the link', () => {
+  it.only('directs the user to the privacy page after removing the target and clicking the link', () => {
     cy.contains('a', 'Política de Privacidade')
-      .invoke('removeAttr', 'target')
-      .click()
+      .should('have.attr','href','privacy.html')
+      .and('have.attr', 'target', '_blank')
 
-    cy.url()
-      .should('be.equal', 'https://cac-tat.s3.eu-central-1.amazonaws.com/privacy.html')
   })
 })
